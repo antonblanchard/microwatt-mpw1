antonblanchard/openlane

Add Microwatt to Caravel
This adds the Microwatt OpenPOWER core to the Caravel project.

openlane/register_file/config.tcl
@@ -1,0 +1,22 @@
+set script_dir [file dirname [file normalize [info script]]]
+
+set ::env(DESIGN_NAME) register_file
+
+set ::env(VERILOG_FILES) "\
+	$script_dir/../../verilog/rtl/register_file.v"
+
+set ::env(CLOCK_PORT) "clk"
+set ::env(CLOCK_PERIOD) "20"
+set ::env(CLOCK_NET) $::env(CLOCK_PORT)
+
+set ::env(DESIGN_IS_CORE) 0
+set ::env(FP_PDN_CORE_RING) 0
+set ::env(GLB_RT_MAXLAYER) 5
+set ::env(GLB_RT_ADJUSTMENT) 0.1
+set ::env(SYNTH_STRATEGY) 2
+set ::env(SYNTH_MAX_FANOUT) 6
+set ::env(FP_CORE_UTIL) 45
+set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
+set ::env(CELL_PAD) 4
+
+set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg

antonblanchard/microwatt-mpw1

openlane/register_file/config.tcl
@@ -1,23 +1,16 @@
 set script_dir [file dirname [file normalize [info script]]]
 
-set ::env(DESIGN_NAME) RAM_512x64
+set ::env(DESIGN_NAME) register_file
 
 set ::env(VERILOG_FILES) "\
-	$script_dir/src/DFFRAM.v
-	$script_dir/src/DFFRAMBB.v
-	$script_dir/src/RAM_512x64.v"
+	$script_dir/../../verilog/rtl/register_file.v"
 
-set ::env(CLOCK_PORT) "CLK"
-set ::env(CLOCK_PERIOD) "10"
+set ::env(CLOCK_PORT) "clk"
+set ::env(CLOCK_PERIOD) "20"
 set ::env(CLOCK_NET) $::env(CLOCK_PORT)
 
-set ::env(CLOCK_TREE_SYNTH) 0
-
-# Required because we are using standard cells
-set ::env(SYNTH_READ_BLACKBOX_LIB) 1
-
 set ::env(FP_SIZING) absolute
-set ::env(DIE_AREA) "0 0 2900 600"
+set ::env(DIE_AREA) "0 0 1100 1100"
 
 # Settings for macros
 set ::env(DESIGN_IS_CORE) 0
@@ -32,16 +25,19 @@ set ::env(VDD_NETS) [list {vccd1} {vccd2} {vdda1} {vdda2}]
 set ::env(GND_NETS) [list {vssd1} {vssd2} {vssa1} {vssa2}]
 
 # Tuning
-set ::env(PL_OPENPHYSYN_OPTIMIZATIONS) 0
-set ::env(PL_TARGET_DENSITY) 0.75
-set ::env(CELL_PAD) 0
+set ::env(PL_TARGET_DENSITY) 0.60
+set ::env(CELL_PAD) 4
+
+set ::env(SYNTH_STRATEGY) "DELAY 2"
 
 set ::env(GLB_RT_L1_ADJUSTMENT) 0.99
-set ::env(GLB_RT_L2_ADJUSTMENT) 0.25
-set ::env(GLB_RT_L3_ADJUSTMENT) 0.25
-set ::env(GLB_RT_L4_ADJUSTMENT) 0.2
+set ::env(GLB_RT_L2_ADJUSTMENT) 0.2
+set ::env(GLB_RT_L3_ADJUSTMENT) 0.2
+set ::env(GLB_RT_L4_ADJUSTMENT) 0.1
 set ::env(GLB_RT_L5_ADJUSTMENT) 0.1
 set ::env(GLB_RT_L6_ADJUSTMENT) 0.1
+
+set ::env(GLB_RT_ALLOW_CONGESTION) 1
 
 set ::env(DIODE_INSERTION_STRATEGY) 3
 